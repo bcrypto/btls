886770cd28e7a808cdecea39e8e35f3e6610afbc
Mikhail Mitskevich <mitskevichmn@gmail.com>
2024-06-04 15:30:25 +0300

    Fix blts ports

--- server/backend/test_btls.py
+++ server/backend/test_btls.py
@@ -24,13 +24,12 @@ home = expanduser("~")
 def test_server_btls(server_):
 	ans = ''
 	print("s_client -connect {} -cipher DHE-BIGN-WITH-BELT-CTR-MAC-HBELT -tls1_2".format(server_))
-	ret_code1, out, err__ = openssl("s_client -connect {} -cipher DHE-BIGN-WITH-BELT-CTR-MAC-HBELT -tls1_2".format(server_), prefix='echo testBTLS |', type_=0)
+	ret_code1, out, err__ = openssl("s_client -connect {}:8443 -cipher DHE-BIGN-WITH-BELT-CTR-MAC-HBELT -tls1_2".format(server_), prefix='echo testBTLS |', type_=0)
 	ans += out.decode('utf-8')
-	ret_code2, out, err__ = openssl("s_client -connect {} -cipher DHE-BIGN-WITH-BELT-DWP-HBELT -tls1_2".format(server_), prefix='echo testBTLS |', type_=0)
+	ret_code2, out, err__ = openssl("s_client -connect {}:8444 -cipher DHE-BIGN-WITH-BELT-DWP-HBELT -tls1_2".format(server_), prefix='echo testBTLS |', type_=0)
 	ans += out.decode('utf-8')
-	ret_code3, out, err__ = openssl("s_client -connect {} -cipher DHT-BIGN-WITH-BELT-CTR-MAC-HBELT -tls1_2".format(server_), prefix='echo testBTLS |', type_=0)
+	ret_code3, out, err__ = openssl("s_client -connect {}:8445 -cipher DHT-BIGN-WITH-BELT-CTR-MAC-HBELT -tls1_2".format(server_), prefix='echo testBTLS |', type_=0)
 	ans += out.decode('utf-8')
-	ret_code4, out, err__ = openssl("s_client -connect {} -cipher DHT-BIGN-WITH-BELT-DWP-HBELT -tls1_2".format(server_), prefix='echo testBTLS |', type_=0)
+	ret_code4, out, err__ = openssl("s_client -connect {}:8446 -cipher DHT-BIGN-WITH-BELT-DWP-HBELT -tls1_2".format(server_), prefix='echo testBTLS |', type_=0)
 	ans += out.decode('utf-8')
-	# result = ret_code1 & ret_code2 & ret_code3 & ret_code4
 	return ret_code1, ret_code2, ret_code3, ret_code4
