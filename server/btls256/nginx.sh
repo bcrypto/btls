59dbd14e57c271e2706736ae3373e812665d6e52
Mikhail Mitskevich <mitskevichmn@gmail.com>
2025-11-18 13:21:00 +0300

    Update server to OpenSSL 3

--- server/btls256/nginx.sh
+++ server/btls256/nginx.sh
@@ -15,10 +15,10 @@ make
 make install
 cd ..
 
-wget https://nginx.org/download/nginx-1.18.0.tar.gz
-tar -zxf nginx-1.18.0.tar.gz
+wget https://nginx.org/download/nginx-1.29.3.tar.gz
+tar -zxf nginx-1.29.3.tar.gz
 #cp ./nginx.patch ./nginx-1.21.3/nginx.patch
-cd nginx-1.18.0
+cd nginx-1.29.3
 #patch -p1 -i nginx.patch
 ls /
 ./configure --pid-path=/usr/local/nginx/nginx.pid --with-pcre=/pcre-8.45 --with-zlib=/zlib-1.3.1 --with-http_ssl_module --with-stream --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --with-ld-opt="-L /opt/usr/local/lib" --with-cc-opt="-I /opt/usr/local/include"
